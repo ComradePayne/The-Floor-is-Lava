51923159221f44a7c4ff450d13db87c77662c35c
ComradePayne <rpierce5@gmu.edu>
2016-04-11 23:41:48 -0400

    Let's try this...

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
+    //Commit bait here!
     "use strict";
     
     var game = new Phaser.Game( 288, 352, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
